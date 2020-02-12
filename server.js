486c081f5615a56d8fd0e7f3498cde63b9e152c8
JayDub21 <justin.wofford.21@gmail.com>
2020-02-11 19:00:42 -0600

    added server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+//                NPM Packages
+//=====================================================
+// Setting Up Port, Models, Express, Routes & MongoDB
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+// Models
+var db = require("./models");
+
+
+// Express
+var app = express();
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+// MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
+
+// Fire It UPP
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
