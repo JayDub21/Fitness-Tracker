dadacf2f3d2eff22ed18b9a6b81dd373bb111578
JayDub21 <justin.wofford.21@gmail.com>
2020-02-14 18:57:48 -0600

    commented out line 4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-require('dotenv').config()
+// require("dotenv").config();
 
 //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 //                NPM Packages
