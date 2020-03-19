6faf321c60d26ff598be9408c3aeeeaa4257c86f
JayDub21 <justin.wofford.21@gmail.com>
2020-03-19 16:58:07 -0500

    fixed stats page + routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,11 @@
 // Requiring our model
 
-const Workout = require("../models/workout");
-// const db = require("../models");
+const db = require("../models");
 
 module.exports = function(app) {
   // Gets All Workouts From DB
   app.get("/api/workouts", (req, res) => {
-    Workout.find({})
+    db.Workout.find({})
       .then(function(response) {
         res.json(response);
       })
@@ -16,8 +15,8 @@ module.exports = function(app) {
   });
 
   // Get workout data
-  app.get("api/workouts/range", (req, res) => {
-    Workout.find({})
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
       .then(function(response) {
         res.json(response);
       })
@@ -29,7 +28,7 @@ module.exports = function(app) {
   // Get single workout by id.
   app.get("/api/workouts/:id", function(req, res) {
     const id = req.params.id;
-    Workout.findById(id, function(err, response) {
+    db.Workout.findById(id, function(err, response) {
       if (err) {
         console.error(err);
       }
@@ -39,7 +38,7 @@ module.exports = function(app) {
 
   app.post("/api/workouts", (req, res) => {
     // const id = req.params.id;
-    Workout.create({ exercise: req.body })
+    db.Workout.create({ exercise: req.body })
       .then(function(response) {
         res.json(response);
       })
@@ -52,7 +51,7 @@ module.exports = function(app) {
   app.put("/api/workouts/:id", (req, res) => {
     console.log(`New Workout: ${req.body}`);
     const wrkt = { _id: req.params.id };
-    Workout.findOneAndUpdate(
+    db.Workout.findOneAndUpdate(
       { _id: req.params.id },
       {
         $push: { exercises: req.body }
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,21 +1,19 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
 
+module.exports = function(app) {
+  // Homepage
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
-module.exports = function (app) {
+  // Exercise Page
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
 
-    // Homepage
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    })
-
-    // Stats Page
-    app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
-    })
-
-    // Exercise Page
-    app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    })
+  // Stats Page
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
 };
