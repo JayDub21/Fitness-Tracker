e3cd0e6910548541fc79bcd2f0169e9054bb4f1f
JayDub21 <Justin.Wofford.21@gmail.com>
2021-02-08 22:47:38 -0600

    require order

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('unifiedTopology', true);
+mongoose.set('useCreateIndex', true);
 
 //↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 //                NPM Packages
@@ -8,11 +11,13 @@ const mongoose = require('mongoose');
 // Setting Up Port, Models, Express, Routes & MongoDB
 //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 var app = express();
 
 require('./models');
+require('./routes/html-routes.js')(app);
+require('./routes/api-routes.js')(app);
 
 // Logger (Must be BELOW express)
 app.use(logger('dev'));
@@ -26,17 +31,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/workout')
   .then(() => console.log('Database is conncted'))
   .catch((err) => console.log(err));
-
-require('./routes/html-routes.js')(app);
-require('./routes/api-routes.js')(app);
 
 // Fire It UPP
 app.listen(PORT, () => {
